2f7522152d097afd6177f66f1ccf5019b519f52e
Vasyl Korzhyk <vasyl.korzhyk@sigma.software>
2020-05-31 12:30:22 +0300

    'Initial'

---
+++ GitHookDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHookDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
